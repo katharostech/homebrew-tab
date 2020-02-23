173acf4debcff0f10c34f7bfc7a103bc73cc8d38
Zicklag <zicklag@katharostech.com>
2020-02-23 18:03:59 +0000

    Update Lucky Pre-Release

--- Casks/lucky-pre-release.rb
+++ Casks/lucky-pre-release.rb
@@ -1,6 +1,6 @@
 cask 'lucky-pre-release' do
     version 'pre-release'
-    sha256 '85ccffb323e0fa099028d8610d590bbd939535bd465a049a2e6715d5ed925ca9'
+    sha256 'dd0bef67fbeb972272cbbe1df62455cc30e63a4591442f4d166392f124089094'
     
     url 'https://github.com/katharostech/lucky/releases/download/pre-release/lucky-mac-x86_64.tgz'
     appcast 'https://github.com/katharostech/lucky/releases.atom'
